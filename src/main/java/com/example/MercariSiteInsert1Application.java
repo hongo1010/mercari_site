c24e8155e92ed52a7da891f3ced6ec9f9c427970
hongo1010 <pttskp1010@gmail.com>
2023-02-07 19:59:23 +0900

    first commit

---
+++ src/main/java/com/example/MercariSiteInsert1Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercariSiteInsert1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercariSiteInsert1Application.class, args);
+	}
+
+}
